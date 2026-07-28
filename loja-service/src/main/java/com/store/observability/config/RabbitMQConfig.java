b2b1275c8539bb1e1cbb216a92225f4b6be69f4c
havillonf <havillonbfreitas@gmail.com>
2026-07-28 17:47:47 -0300

    feature: create RabbitMQ config file

---
+++ loja-service/src/main/java/com/store/observability/config/RabbitMQConfig.java
@@ -0,0 +1,23 @@
+package com.store.observability.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_NAME = "metrics.queue";
+
+    @Bean
+    public Queue metricsQueue() {
+        return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
